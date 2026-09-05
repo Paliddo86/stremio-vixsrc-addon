73a7a3119333ad12ddd5d710ffff4c2a5de8653d
Paliddo86 <L.pallante86@gmail.com>
2026-09-05 15:29:05 +0200

    Ensure manifest returns proper headers (CORS preflight, Content-Type) and updated version

--- api/manifest.js
+++ api/manifest.js
@@ -1,11 +1,21 @@
 module.exports = (req, res) => {
+  // Respond to CORS preflight
+  if (req.method === 'OPTIONS') {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.statusCode = 204;
+    return res.end();
+  }
+
   const manifest = {
     id: 'org.paliddo86.stremio.vixsrc',
-    version: '0.2.1',
+    version: '1.0.0',
     name: 'Stremio Vixsrc Addon (integrated)',
     description: "Addon che usa vixsrc.to per trovare stream. Usa l'id passato da Stremio (non serve TMDB/IMDB mapping).",
     resources: ['stream', 'meta'],
     types: ['movie', 'series'],
+    // Include common id prefixes used by Stremio
     idPrefixes: ['imdb:', 'tmdb:', 'vix:', 'tt'],
     catalogs: [],
     behaviorHints: { disableTrackers: true }
