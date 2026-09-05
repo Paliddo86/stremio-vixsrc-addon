84c74dcda68a226857e9cb3f0cda487e37a84e83
Paliddo86 <L.pallante86@gmail.com>
2026-09-05 15:25:12 +0200

    Improve manifest: add CORS header, catalogs, behaviorHints and update version

--- api/manifest.js
+++ api/manifest.js
@@ -1,12 +1,17 @@
 module.exports = (req, res) => {
-  res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify({
+  const manifest = {
     id: 'org.paliddo86.stremio.vixsrc',
-    version: '0.2.0',
+    version: '0.2.1',
     name: 'Stremio Vixsrc Addon (integrated)',
-    description: 'Addon che usa vixsrc.to per trovare stream. Usa l\'id passato da Stremio (non serve TMDB/IMDB mapping).',
+    description: "Addon che usa vixsrc.to per trovare stream. Usa l'id passato da Stremio (non serve TMDB/IMDB mapping).",
     resources: ['stream', 'meta'],
     types: ['movie', 'series'],
-    idPrefixes: ['imdb:', 'tmdb:', 'vix:']
-  }));
+    idPrefixes: ['imdb:', 'tmdb:', 'vix:', 'tt'],
+    catalogs: [],
+    behaviorHints: { disableTrackers: true }
+  };
+
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.end(JSON.stringify(manifest));
 };
